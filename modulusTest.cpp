4c6a5025f6940171e054b696b57316d360d33484
zachj555 <zachj555@users.noreply.github.com>
2021-02-23 14:51:29 -0800

    Keeping up to date with RsaUI

--- modulusTest.cpp
+++ modulusTest.cpp
@@ -1,20 +1,19 @@
 // modulusTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
 #include "RsaUI.h"
 
-uint64_t FindD(uint64_t exp, uint64_t mod);
+//unsigned long long FindD(unsigned long long exp, unsigned long long mod);
 
 int main()
 {
-    //int base = 2, exponent = 113971301, modulus = 210506400;
+    /*int base = 2, exponent = 113971301, modulus = 210506400;*/
 
 
-	/// <summary>
-	/// Performs Encryption/decryption
-	/// </summary>
-	/// <returns> uint64_t </returns>
+	 /*<summary>
+	 Performs Encryption/decryption
+	 </summary>
+	 <returns> uint64_t </returns>*/
     
 	/*int result = 1;
 	for (int i = 0; i < exponent; i++)
@@ -33,59 +32,59 @@ int main()
         RsaUI ui;
         //Generate new RSA class, and key sets
         //Call RsaUI functions setChoice, setExponent, setN, and relevant setText function
-            //in order to pass the values to the UI class
+        ///////in order to pass the values to the UI class
         //Run program's encryption/decryption, based off of user choice
 
         if (ui.getChoice() == 'e')
             //do encryption
             //make sure to set the ciphertext in RsaUI after it is encrypted
-            std::cout << "Your cipher text is: " << ui.getCipherText() << std::endl;
+            cout << "Your cipher text is: " << ui.getCipherText() << endl;
         else
         {
             //do decryption
             //make sure to set the plaintext in RsaUI after it is decrypted
-            std::cout << "Your plaintext is: " << ui.getPlainText() << std::endl;
+            cout << "Your plaintext is: " << ui.getPlainText() << endl;
         }    
 
-        std::cout << "Would you like to run the program again(y/n)? " << std::endl;
-        std::cin >> cont;
+        cout << "Would you like to run the program again(y/n)? " << endl;
+        cin >> cont;
         cont = tolower(cont);
         while (cont != 'y' && cont != 'n')
         {
-            std::cout << "Improper input, please input either 'y' or 'n' (without quotations): " << std::endl;
-            std::cin >> cont;
+            cout << "Improper input, please input either 'y' or 'n': " << endl;
+            cin >> cont;
             cont = tolower(cont);
         }
     }
 
     return 0;
 }
-
-uint64_t FindD(uint64_t exp, uint64_t mod)
-{
-    uint64_t d, exp1, exp3, mod1, mod3, totient1, totient3, q;
-    int64_t iter;
-    exp1 = 1;
-    exp3 = exp;
-    mod1 = 0;
-    mod3 = mod;
-    iter = 1;
-    while (mod3 != 0)
-    {
-        q = exp3 / mod3;
-        totient3 = exp3 % mod3;
-        totient1 = exp1 + q * mod1;
-        exp1 = mod1;
-        mod1 = totient1;
-        exp3 = mod3;
-        mod3 = totient3;
-        iter = -iter;
-    }
-    if (exp3 != 1)
-        return  0;
-    if (iter < 0)
-        d = mod - exp1;
-    else
-        d = exp1;
-    return d;
-}
+//
+//unsigned long long FindD(unsigned long long exp, unsigned long long mod)
+//{
+//    unsigned long long d, exp1, exp3, mod1, mod3, totient1, totient3, q;
+//    long long iter;
+//    exp1 = 1;
+//    exp3 = exp;
+//    mod1 = 0;
+//    mod3 = mod;
+//    iter = 1;
+//    while (mod3 != 0)
+//    {
+//        q = exp3 / mod3;
+//        totient3 = exp3 % mod3;
+//        totient1 = exp1 + q * mod1;
+//        exp1 = mod1;
+//        mod1 = totient1;
+//        exp3 = mod3;
+//        mod3 = totient3;
+//        iter = -iter;
+//    }
+//    if (exp3 != 1)
+//        return  0;
+//    if (iter < 0)
+//        d = mod - exp1;
+//    else
+//        d = exp1;
+//    return d;
+//}
