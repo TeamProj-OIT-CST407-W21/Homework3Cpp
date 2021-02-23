e83dd63220b5e948481983871c29f5927c2233e9
zachj555 <zachj555@users.noreply.github.com>
2021-02-23 02:00:40 -0800

    Adding continuation loop
    Also stubbed out the if-else that will be used to run either encrypt or decrypt depending on user input.
    
    Commented out previous code that was used for testing

--- modulusTest.cpp
+++ modulusTest.cpp
@@ -2,19 +2,21 @@
 //
 
 #include <iostream>
+#include "RsaUI.cpp"
 
 uint64_t FindD(uint64_t exp, uint64_t mod);
 
 int main()
 {
-    int base = 2, exponent = 113971301, modulus = 210506400;
+    //int base = 2, exponent = 113971301, modulus = 210506400;
 
 
 	/// <summary>
 	/// Performs Encryption/decryption
 	/// </summary>
 	/// <returns> uint64_t </returns>
-	int result = 1;
+    
+	/*int result = 1;
 	for (int i = 0; i < exponent; i++)
 		result = (result * base) % modulus;
 
@@ -22,7 +24,39 @@ int main()
 
 
     uint64_t d = FindD(exponent, modulus);
-    std::cout << "D is: " << d << std::endl;
+    std::cout << "D is: " << d << std::endl;*/
+
+    char cont = 'y';
+
+    while (cont == 'y')
+    {
+        RsaUI ui;
+        //Generate new RSA class, and key sets
+        //Call RsaUI functions setChoice, setExponent, setN, and relevant setText function
+            //in order to pass the values to the UI class
+        //Run program's encryption/decryption, based off of user choice
+
+        if (ui.getChoice() == 'e')
+            //do encryption
+            //make sure to set the ciphertext in RsaUI after it is encrypted
+            std::cout << "Your cipher text is: " << ui.getCipherText() << std::endl;
+        else
+        {
+            //do decryption
+            //make sure to set the plaintext in RsaUI after it is decrypted
+            std::cout << "Your plaintext is: " << ui.getPlainText() << std::endl;
+        }    
+
+        std::cout << "Would you like to run the program again(y/n)? " << std::endl;
+        std::cin >> cont;
+        cont = tolower(cont);
+        while (cont != 'y' && cont != 'n')
+        {
+            std::cout << "Improper input, please input either 'y' or 'n' (without quotations): " << std::endl;
+            std::cin >> cont;
+            cont = tolower(cont);
+        }
+    }
 
     return 0;
 }
